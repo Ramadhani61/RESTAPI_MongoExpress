dc4c5b15d139a130dffc2f3fa6687bc34f3d7910
imramadhani01@gmail.com <imramdhani01@gmail.com>
2023-04-03 13:05:16 +0700

    koneksi to mongoDB

--- app.js
+++ app.js
@@ -2,12 +2,30 @@ const express = require('express');
 const cors = require('cors');
 
 const app= express();
-
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+
+const db = require('./app/models')
+
+db.mongoose.connect(db.url,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}).then(()=>{
+    console.log("berhasil conect");
+}).catch((err)=>{
+    console.log("error",err);
+    process.exit();
+})
+
+app.get('/',(req,res)=>{
+    res.json({
+        message:"hallo guys"
+    })
+})
 
 const PORT = 8000;
 
 app.listen(PORT ,()=> {
     console.log(`SERVER IS RUNNING IS PORT ${PORT}`);
+    
 })
---
+++ config/db.config.js
@@ -0,0 +1,3 @@
+module.exports={
+    url:"mongodb+srv://wakansfood01:mongowaka@cluster0.pxcq1x7.mongodb.net/collect_tech"
+}
